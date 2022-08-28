0ac0ace50755337afcfb3032b5f145ea664ca9d9
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-28 19:33:25 +0300

    Показать индекс или порядковый номер по нужному нам значению

---
+++ Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 12, 18, 31, 24, 4, 14, 18 };
+
+int n = array.Length; // указываем длину массива.
+
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if ( array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
