77d7c2519ccbc4fc5b42876ffcd5ec4917acdb51
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-28 14:58:29 +0300

    Деление

---
+++ Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// частное двух значений
+// int для целых чисел. double для вещественных чисел!!!
+double numberA = 12;
+double numberB = 5;
+double result = numberA / numberB;
+
+Console.Write(result);
+
