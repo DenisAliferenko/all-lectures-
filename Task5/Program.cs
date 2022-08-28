7c76e05e081ccd81bfa58568ef3e840518e1d270
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-28 15:12:14 +0300

    Рандомное число

---
+++ Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿// если мы хотим получить рандомное число можно ввести
+int numberA = new Random().Next(1, 10); // даст любое число от 1 до 9
+Console.WriteLine(numberA);
+
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+
+int result = numberA + numberB;
+Console.WriteLine(result);
