327104dcf0ed4fbec46d3fd7072e8253c80ab2ab
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-28 15:23:13 +0300

    Приветсвие пользователя

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// приветствуем любимчика)
+// программа работает на ввод и вывод данных с данными пользователя.
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
