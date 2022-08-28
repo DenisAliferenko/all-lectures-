123ce5d74a06b586a344b2a6f01fbd56d018824a
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-28 14:40:39 +0300

    sum of two numbers

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Сложение двух чисел на языке C#
+// мы четко должны понимать откуда берутся данные! и т.д
+int numberA = 3;  // int исползуют для целых чисел.
+int numberB = 6; 
+// можно через дополнительную переменную
+// int result = numberA + numberB
+// Console.WriteLine(result);
+Console.WriteLine(numberA+numberB);
