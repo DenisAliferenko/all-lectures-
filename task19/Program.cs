9f13ce60b7e7471c13d2c7bab4d56e4eda21b573
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-25 21:34:26 +0300

    двумерные массивы через метод

---
+++ task19/Program.cs
@@ -0,0 +1,30 @@
+﻿// двумерная массив но через метод уже печатаем.
+
+void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++ )
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+       Console.Write($" {matr [i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+    for(int i =0; i < matr.GetLength(0); i++)
+    {
+        for(int j =0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+int[,] matrix1 = new int[3, 4];
+PrintArray(matrix1);
+FillArray(matrix1);
+Console.WriteLine();
+
+PrintArray(matrix1);
